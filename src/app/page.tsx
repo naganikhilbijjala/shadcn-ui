07122ca9b4d68396d6d0204f94f0d6b2d2368589
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2024-12-29 16:18:16 -0500

    feat: add card component. to show recipes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,52 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Recipe {
+  title: string,
+  image: string,
+  time: number,
+  description: string,
+  vegan: boolean,
+  id: string
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const res = await fetch('http://localhost:4000/recipes')
+  return res.json()
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
   return (
     <main>
-      home
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            {/* <img src={recipe.image} alt={recipe.title} /> */}
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
+
